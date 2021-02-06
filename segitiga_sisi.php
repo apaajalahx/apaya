3098a43dc3097058aa33af44dc8d1176a1e7199f
Jonathan Jason <52324987+apaajalahx@users.noreply.github.com>
2021-02-06 20:46:46 +0700

    Change

--- segitiga_sisi.php
+++ segitiga_sisi.php
@@ -2,5 +2,5 @@
 //code by Dinar Hamid. 
 $s=20; // you can change this value
 for($i=0;$i<=$s;$i++){
-echo str_repeat("\x20",$s--).substr(str_shuffle(str_repeat('@#&()=%',$i)),0,$i++)."\n";
+echo str_repeat("\x20",$s--).substr(str_repeat('#',$i),0,$i++)."\n";
 }
