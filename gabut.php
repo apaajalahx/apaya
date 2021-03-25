948e7980a4cb64b99279c70989a2fa7dd7de1d67
Dinar Hamid <52324987+apaajalahx@users.noreply.github.com>
2021-03-25 07:13:28 +0700

    Update gabut.php

--- gabut.php
+++ gabut.php
@@ -1,16 +1,23 @@
 <?php
-// created by dinar hamid
-// gabut coy woekwoekoke
+/**
+ * @author Dinar Hamid
+ * atas ke bawah
+ * sqrt(x)*y-y
+ * loop from 0 to result sqrt 
+ * ambil tengah dan lakukan decrement
+ * (input-sqrt)/2+sqrt
+ */
+$a = $argv[1]; // php gabut.php 16
 $u = 1;
 $x = 1;
-for($i=0;$i<=10;$i++){
+for($i=0;$i<=(sqrt($a)*2-2);$i++){
     for($j=0;$j<=$x-1;$j++){
         echo $u++." ";
     }
-    if($u>21){
+    if($u>($a - sqrt($a))/2+sqrt($a)){
         $x--;
     } else {
         $x++;
-    }
+}
     echo "\n";
 }
