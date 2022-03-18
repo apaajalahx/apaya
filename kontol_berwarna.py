949e5dd51b1fc2d3fcc3a7231e1a382fd32b81ec
Dinar Hamid <52324987+apaajalahx@users.noreply.github.com>
2022-03-18 15:01:30 +0700

    Create kontol_berwarna.py

---
+++ kontol_berwarna.py
@@ -0,0 +1,20 @@
+from termcolor import colored
+import random
+
+z = ( ['K','k'] , ['O','o'], ['N','n'],['T','t'],['O','o'],['L','l'])
+o = 0
+warna = ['cyan','red','blue','white','green','yellow']
+try:
+    while True:
+        y = ''
+        for i in range(0,len(z)):
+            p = random.choice(warna)
+            u = 0 if o == i else 1
+            y += f"{colored(z[i][u],p)}"
+        print(f"{y}", end="\r")
+        o = o + 1
+        if o == 6:
+            o = 0
+except KeyboardInterrupt:
+    print('end', end="\r")
+    
