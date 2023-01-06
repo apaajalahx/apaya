342f2f60b20294420605d8f7e64698b86c1ba51d
Dinar Hamid <52324987+apaajalahx@users.noreply.github.com>
2023-01-07 02:51:12 +0700

    Update parent_child.py

--- parent_child.py
+++ parent_child.py
@@ -1,19 +1,25 @@
 # Python Parent and Child.
-# call parent name.
-# call child name.
 
 class A:
     
     def print_child_class_name(self):
-        print(self.__class__.__name__)
+        print('Child Class', self.__class__.__name__)
 
+class C:
+    pass
 
-class B(A):
+class B(A, C):
 
     def print_parent_class_name(self):
-        print(self.__class__.__base__.__name__)
+        print('Single Parent Class', self.__class__.__base__.__name__)
+    
+    def print_multiple_parent_class_name(self):
+        for classes in self.__class__.__bases__:
+            print('Multiple Class {}'.format(classes.__name__))
+
 
 
 getClass = B() 
 getClass.print_child_class_name()
 getClass.print_parent_class_name()
+getClass.print_multiple_parent_class_name()
